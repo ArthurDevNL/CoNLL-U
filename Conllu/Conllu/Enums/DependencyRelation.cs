f75e48d3833ce3fe31e0ea1f005bd3fa1e0c8308
Arthur Hemmer <arthurhemmer@hotmail.com>
2021-10-31 11:42:09 +0100

    Improved comments
    Updated gitignore

--- Conllu/Conllu/Enums/DependencyRelation.cs
+++ Conllu/Conllu/Enums/DependencyRelation.cs
@@ -6,42 +6,151 @@ namespace Conllu.Enums
     /// </summary>
     public enum DependencyRelation
     {
-        Acl, // clausal modifier of noun (adjectival clause)
-        Advcl, // adverbial clause modifier
-        Advmod, // adverbial modifier
-        Amod, // adjectival modifier
-        Appos, // appositional modifier
-        Aux, // auxiliary
-        Case, // case marking
-        Cc, // coordinating conjunction
-        Ccomp, // clausal complement
-        Clf, // classifier
-        Compound, // compound
-        Conj, // conjunct
-        Cop, // copula
-        Csubj, // clausal subject
-        Dep, // unspecified dependency
-        Det, // determiner
-        Discourse, // discourse element
-        Dislocated, // dislocated elements
-        Expl, // expletive
-        Fixed, // fixed multiword expression
-        Flat, // flat multiword expression
-        Goeswith, // goes with
-        Iobj, // indirect object
-        List, // list
-        Mark, // marker
-        Nmod, // nominal modifier
-        Nsubj, // nominal subject
-        Nummod, // numeric modifier
-        Obj, // object
-        Obl, // oblique nominal
-        Orphan, // orphan
-        Parataxis, // parataxis
-        Punct, // punctuation
-        Reparandum, // overridden disfluency
-        Root, // root
-        Vocative, // vocative
-        Xcomp, // open clausal complement
+        /// <summary>
+        /// Clausal modifier of noun (adjectival clause)
+        /// </summary>
+        Acl,
+        /// <summary>
+        /// adverbial clause modifier
+        /// </summary>
+        Advcl, // 
+        /// <summary>
+        /// Adverbial modifier
+        /// </summary>
+        Advmod, //
+        /// <summary>
+        /// Adjectival modifier
+        /// </summary>
+        Amod, // 
+        /// <summary>
+        /// Appositional modifier
+        /// </summary>
+        Appos, //
+        /// <summary>
+        /// Auxiliary
+        /// </summary>
+        Aux, // 
+        /// <summary>
+        /// Case marking
+        /// </summary>
+        Case, //
+        /// <summary>
+        /// Coordinating conjunction
+        /// </summary>
+        Cc, 
+        /// <summary>
+        /// Clausal complement
+        /// </summary>
+        Ccomp,
+        /// <summary>
+        /// Classifier
+        /// </summary>
+        Clf, 
+        /// <summary>
+        /// Compound
+        /// </summary>
+        Compound,
+        /// <summary>
+        /// Conjunct
+        /// </summary>
+        Conj,
+        /// <summary>
+        /// Copula
+        /// </summary>
+        Cop,
+        /// <summary>
+        ///  Clausal subject
+        /// </summary>
+        Csubj,
+        /// <summary>
+        /// Unspecified dependency
+        /// </summary>
+        Dep,
+        /// <summary>
+        /// Determiner
+        /// </summary>
+        Det,
+        /// <summary>
+        /// Discourse element
+        /// </summary>
+        Discourse,
+        /// <summary>
+        /// Dislocated elements
+        /// </summary>
+        Dislocated,
+        /// <summary>
+        /// Expletive
+        /// </summary>
+        Expl,
+        /// <summary>
+        /// Fixed multiword expression
+        /// </summary>
+        Fixed,
+        /// <summary>
+        /// Flat multiword expression
+        /// </summary>
+        Flat,
+        /// <summary>
+        ///  Goes with
+        /// </summary>
+        Goeswith,
+        /// <summary>
+        /// Indirect object
+        /// </summary>
+        Iobj,
+        /// <summary>
+        /// List
+        /// </summary>
+        List,
+        /// <summary>
+        /// Marker
+        /// </summary>
+        Mark,
+        /// <summary>
+        /// Nominal modifier
+        /// </summary>
+        Nmod,
+        /// <summary>
+        /// Nominal subject
+        /// </summary>
+        Nsubj,
+        /// <summary>
+        /// Numeric modifier
+        /// </summary>
+        Nummod,
+        /// <summary>
+        /// Object
+        /// </summary>
+        Obj,
+        /// <summary>
+        /// Oblique nominal
+        /// </summary>
+        Obl,
+        /// <summary>
+        /// Orphan
+        /// </summary>
+        Orphan,
+        /// <summary>
+        /// Parataxis
+        /// </summary>
+        Parataxis,
+        // Punctuation
+        Punct,
+        /// <summary>
+        /// Overridden disfluency
+        /// </summary>
+        Reparandum,
+        /// <summary>
+        /// Root
+        /// </summary>
+        Root,
+        /// <summary>
+        /// Vocative
+        /// </summary>
+        Vocative,
+        /// <summary>
+        /// Open clausal complement
+        /// </summary>
+        Xcomp
     }
 }
--- Conllu/Conllu/Enums/PosTag.cs
+++ Conllu/Conllu/Enums/PosTag.cs
@@ -2,22 +2,73 @@ namespace Conllu.Enums
 {
     public enum PosTag
     {
-        Adj, // adjective
-        Adp, // adposition
-        Adv, // adverb
-        Aux, // auxiliary
-        Cconj, // coordinating conjunction
-        Det, // determiner
-        Intj, // interjection
-        Noun, // noun
-        Num, // numeral
-        Part, // particle
-        Pron, // pronoun
-        Propn, // proper noun
-        Punct, // punctuation
-        Sconj, // subordinating conjunction
-        Sym, // symbol
-        Verb, // verb
-        X, // other
+        /// <summary>
+        /// Adjective
+        /// </summary>
+        Adj,
+        /// <summary>
+        /// Adposition
+        /// </summary>
+        Adp,
+        /// <summary>
+        /// Adverb
+        /// </summary>
+        Adv,
+        /// <summary>
+        /// Auxiliary
+        /// </summary>
+        Aux,
+        /// <summary>
+        /// Coordinating conjunction
+        /// </summary>
+        Cconj,
+        /// <summary>
+        /// Determiner
+        /// </summary>
+        Det,
+        /// <summary>
+        /// Interjection
+        /// </summary>
+        Intj,
+        /// <summary>
+        /// Noun
+        /// </summary>
+        Noun,
+        /// <summary>
+        /// Numeral
+        /// </summary>
+        Num,
+        /// <summary>
+        /// Particle
+        /// </summary>
+        Part,
+        /// <summary>
+        /// Pronoun
+        /// </summary>
+        Pron,
+        /// <summary>
+        /// Proper noun
+        /// </summary>
+        Propn,
+        /// <summary>
+        /// Punctuation
+        /// </summary>
+        Punct,
+        /// <summary>
+        /// Subordinating conjunction
+        /// </summary>
+        Sconj,
+        /// <summary>
+        /// Symbol
+        /// </summary>
+        Sym,
+        /// <summary>
+        /// Verb
+        /// </summary>
+        Verb,
+        /// <summary>
+        /// Other
+        /// </summary>
+        X,
     }
 }
